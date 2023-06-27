24ec5e46095ad7fb67e691b0d3c3786ef98df2df
Addit <adam.garza33@hotmail.com>
2023-06-26 23:20:58 -0500

    resolver function to get logged in user, Mutation object started

---
+++ server/schemas/resolver.js
@@ -0,0 +1,42 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+const resolver = {
+    Query: {
+        // get the logged in user from context and find user details in the db
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({_id: context.user._id});
+            }
+            throw new AuthenticationError('Log in required');
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            // Find the user with the given email
+            const user = await User.findOne({ email });
+            
+            // If no user is found, throw an AuthenticationError
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+              }
+
+              // Checks if provided password is correct
+              const correctPw = await user.isCorrectPassword(password);
+        
+              // If the password is incorrect, throw an AuthenticationError
+              if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+              }
+        
+               // Generate a token for the authenticated user
+              const token = signToken(user);
+              
+              // return token and user object
+              return { token, user };
+    },
+
+    
+};
