5836e7fc964b37710f80a815cf5cd2dff4c32443
Addit <adam.garza33@hotmail.com>
2023-06-27 01:08:48 -0500

    Create an Apollo provider to make every request work w/ the apollo server

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,34 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// HttpLink that is the terminating link
+// endpoint of GraphQL server as its URL
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+// new Apollo client instance, pass in authLink, httpLink, and cache the object
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to
+  // our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
